7122859853fd725d0f852bef0ec7a6f7850b10f6
Sérgio Silveira <sdsilveira@gmail.com>
2022-07-25 16:23:04 +0200

    omg it was the token

--- src/Krimson.Core/Components/Readers/KrimsonReader.cs
+++ src/Krimson.Core/Components/Readers/KrimsonReader.cs
@@ -123,10 +123,11 @@ public sealed class KrimsonReader : IKrimsonReaderInfo {
 
             using var recordReaderCancellator = CancellationTokenSource.CreateLinkedTokenSource(cancellator.Token);
 
-            await foreach (var record in consumer.Records(_ => recordReaderCancellator.Cancel(), cancellator.Token).ConfigureAwait(false))
+            await foreach (var record in consumer.Records(_ => recordReaderCancellator.Cancel(), recordReaderCancellator.Token).ConfigureAwait(false)) {
                 yield return record;
-
-            recordReaderCancellator.Cancel();
+                recordReaderCancellator.Cancel();
+            }
+            
             consumer.Unassign();
         }
         
