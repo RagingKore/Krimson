d095623e06530e3730e253bee02b9d87251d2661
Sérgio Silveira <sdsilveira@gmail.com>
2022-07-26 09:57:30 +0200

    empty source record

--- src/Krimson.Connectors/SourceRecord.cs
+++ src/Krimson.Connectors/SourceRecord.cs
@@ -3,6 +3,15 @@ using Google.Protobuf.WellKnownTypes;
 namespace Krimson.Connectors;
 
 public partial class SourceRecord {
+    public static readonly SourceRecord Empty = new() {
+        Id        = string.Empty,
+        Data      = new Struct(),
+        Timestamp = Timestamp.FromDateTimeOffset(DateTimeOffset.MinValue),
+        Type      = string.Empty,
+        Operation = SourceOperation.Unspecified,
+        Source    = null
+    };
+
     public static SourceRecord From(string id, string type, DateTimeOffset timestamp, Struct data, string? source = null) {
         return new SourceRecord {
             Id        = id,
