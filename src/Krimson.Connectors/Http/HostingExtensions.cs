fcb42d4f7f80e20275a976cbe507a85de7473a4c
Richard Tasker <ritasker8t2@gmail.com>
2022-08-17 16:50:13 +0100

    Switch to use .As<>()

--- src/Krimson.Connectors/Http/HostingExtensions.cs
+++ src/Krimson.Connectors/Http/HostingExtensions.cs
@@ -29,7 +29,7 @@ public static class ServicesExtensions {
             .FromAssemblyOf<T>()
             .AddClasses(classes => classes.AssignableTo<WebhookSourceConnector>())
             .UsingRegistrationStrategy(RegistrationStrategy.Skip)
-            .AsSelfWithInterfaces()
+            .As<WebhookSourceConnector>()
             .WithSingletonLifetime()
         );
 
