946c60fe644a00ebde1fe75dea220cc990784b07
Sérgio Silveira <sdsilveira@gmail.com>
2022-08-22 18:33:04 +0200

    fixed webhook source connectors service registrations made webhook paths mandatory upgraded packages

--- src/Krimson.Connectors/Http/HostingExtensions.cs
+++ src/Krimson.Connectors/Http/HostingExtensions.cs
@@ -1,12 +1,9 @@
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Scrutor;
-
 namespace Krimson.Connectors.Http;
 
 public static class ServicesExtensions {
     public static IServiceCollection AddKrimsonWebhookSourceConnectors(this IServiceCollection services) {
         services.AddKrimsonReader();
- 
+
         services.Scan(
             scan => scan.FromApplicationDependencies()
                 .AddClasses(
@@ -14,35 +11,40 @@ public static class ServicesExtensions {
                         .AssignableTo<WebhookSourceConnector>()
                         .NotInNamespaceOf<WebhookSourceConnector>()
                 )
-                .UsingRegistrationStrategy(RegistrationStrategy.Skip)
-                .AsSelfWithInterfaces()
+                .As<WebhookSourceConnector>()
+                .AsImplementedInterfaces()
+                .AsSelf()
                 .WithSingletonLifetime()
         );
 
         return services;
     }
-
+    
     public static IServiceCollection AddKrimsonWebhookSourceConnector<T>(this IServiceCollection services) where T: WebhookSourceConnector {
         services.AddKrimsonReader();
 
-        services.Scan(scan => scan
-            .FromAssemblyOf<T>()
-            .AddClasses(classes => classes.AssignableTo<WebhookSourceConnector>())
-            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
-            .AsSelfWithInterfaces()
-            .WithSingletonLifetime()
+        services.Scan(
+            scan => scan.FromAssemblyOf<T>()
+                .AddClasses(classes => classes.AssignableTo<WebhookSourceConnector>())
+                .As<WebhookSourceConnector>()
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .WithSingletonLifetime()
         );
-
+    
         return services;
     }
 }
 
 public static class WebApplicationExtensions {
     public static WebApplication UseKrimsonWebhookSourceConnectors(this WebApplication app) {
-        foreach (var source in app.Services.GetServices<WebhookSourceConnector>()) {
-            var webhookPath = GetWebhookPath(source.GetType());
+        foreach (var connector in app.Services.GetServices<WebhookSourceConnector>()) {
+            var webhookPath = GetWebhookPath(connector.GetType());
             
-            app.MapPost(webhookPath, async http => await source.Process(new(http)).ConfigureAwait(false));
+            if (string.IsNullOrWhiteSpace(webhookPath))
+                throw new($"{connector.GetType().Name} webhook path is missing or undefined");
+            
+            app.MapPost(webhookPath, async http => await connector.Process(new(http)).ConfigureAwait(false));
         }
         
         return app;
--- src/Krimson.Connectors/PullSource/HostingExtensions.cs
+++ src/Krimson.Connectors/PullSource/HostingExtensions.cs
@@ -1,20 +1,19 @@
-using Scrutor;
-
 namespace Krimson.Connectors;
 
 [PublicAPI]
 public static class ServicesExtensions {
     public static IServiceCollection AddKrimsonPullSourceConnector<T>(this IServiceCollection services, TimeSpan? backoffTime = null) where T : PullSourceConnector {
         services.AddKrimsonReader();
- 
-        services.Scan(scan => scan
-            .FromAssemblyOf<T>()
-            .AddClasses(classes => classes.AssignableTo<PullSourceConnector>())
-            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
-            .AsSelfWithInterfaces()
-            .WithSingletonLifetime()
+
+        services.Scan(
+            scan => scan
+                .FromAssemblyOf<T>()
+                .AddClasses(classes => classes.AssignableTo<PullSourceConnector>())
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .WithSingletonLifetime()
         );
-        
+
         return backoffTime is null
             ? services.AddHostedService<PullSourceConnectorHost<T>>()
             : services.AddHostedService(ctx => new PullSourceConnectorHost<T>(ctx.GetRequiredService<T>(), ctx, backoffTime));
